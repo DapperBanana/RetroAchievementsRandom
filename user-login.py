41adc1c82497092b1ca9f6c6ebf048ecf7c0ce44
Austin L. Howard <austin.lane.howard@gmail.com>
2023-10-31 13:45:04 -0600

    Update user-login.py

--- user-login.py
+++ user-login.py
@@ -1,22 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Create a session for making HTTP requests
 session = requests.Session()
 
-# Step 0: Set up the user credentials
-UserName = 'TestUser9989'
-Password = 'MailMan9989!'
-User_API_Key = ''
+# Set up user credentials
+UserName = 'TestUser9989'  # Replace with your username
+Password = 'MailMan9989!'  # Replace with your password
+User_API_Key = ''  # Initialize an empty API key
 
-# Step 1: Fetch the login page
+# Fetch the login page
 login_url = 'https://retroachievements.org/login'
 response = session.get(login_url)
 
-# Step 2: Parse the CSRF token
+# Parse the CSRF token from the login page
 soup = BeautifulSoup(response.text, 'html.parser')
 csrf_token = soup.find('input', {'name': '_token'})['value']
 
-# Step 3: Perform login
+# Perform login
 login_data = {
     '_token': csrf_token,
     'User': UserName,
@@ -24,24 +25,23 @@ login_data = {
 }
 login_response = session.post(login_url, data=login_data, allow_redirects=True)
 
-# Step 4: Fetch the profile page
+# Fetch the user profile page
 profile_url = 'https://retroachievements.org/controlpanel.php'
 profile_response = session.get(profile_url)
 
-# Step 5: Parse the profile page
+# Parse the user profile page
 profile_soup = BeautifulSoup(profile_response.text, 'html.parser')
 
-# Using the CSS selector from the JS path
+# Using a CSS selector to extract specific information
 element = profile_soup.select_one("body > div:nth-child(5) > main > article > div > div:nth-child(4) > table > tbody > tr:nth-child(1) > td:nth-child(2) > input")
 value = element['value'] if element else None
 
-
-
+# Check if the login was successful
 if value:
     print("Successful login!")
     User_API_Key = value
     print("Username: " + UserName)
-    print("Password: " + Password)
+    print("Password: " + Password)  # Note: Printing the password is not recommended in practice.
     print("Web API Key: " + User_API_Key)
 else:
     print("Incorrect credentials...")
